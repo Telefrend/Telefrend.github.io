323b14acc52aa85c4d4ea0848cc95a9dd27d3bf6
Onenemanuel <onenemanuel@gmail.com>
2022-06-24 17:16:35 +0300

    Changed scroll events for the body

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+let isScrolling = false;
+
 const body = document.querySelector(".body");
 const header = document.querySelector(".header");
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
+window.addEventListener("scroll", function () {
+  header.style.display = "none";
+});
+
 body.addEventListener("touchstart", () => {
   header.style.display = "block";
-
   setTimeout(function () {
     header.style.display = "none";
   }, 3000);
