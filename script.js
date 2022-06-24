cecab4b1841107f848f854e9f6123e8c3bcdc933
Onenemanuel <onenemanuel@gmail.com>
2022-06-24 16:28:10 +0300

    Changed navbar apperance

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const body = document.querySelector(".body");
+const header = document.querySelector(".header");
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+body.addEventListener("touchstart", () => {
+  header.style.display = "block";
+
+  setTimeout(function () {
+    header.style.display = "none";
+  }, 3000);
+});
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
